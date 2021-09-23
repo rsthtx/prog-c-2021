c2e2c23d9a2117567aabdc496d37b3f89a55e85b
Rasmus Stougaard <rst@eucnordvest.dk>
2021-09-23 14:19:52 +0200

    Add bouncing demo

--- bouncing-ball-demo/sketch.js
+++ bouncing-ball-demo/sketch.js
@@ -1,16 +1,48 @@
-let xPos = 40;
+let xPos;
+let yPos = 250
+let xSpeed
+let ySpeed
+
+
+// https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/2560px-DVD_logo.svg.png
+let img;
+function preload() {
+  let imgageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/DVD_logo.svg/2560px-DVD_logo.svg.png"
+  img = loadImage(imgageUrl);
+}
 
 function setup() {
-  createCanvas(400, 500)
+  createCanvas(windowWidth, windowHeight)
+  xSpeed = 7
+  ySpeed = 5
+  xPos = width / 2
+  background(220);
 }
 
 function draw() {
-  background(220);
-  
-  circle(xPos, 200, 50)
-  
-  // xPos = xPos + 1
-  xPos  += 1
-}
 
-Hold B har dansk i 2-4 lektion i lokale 4005 i stedet for 4101
+  let radius = 100
+  let diameter = 2 * radius
+
+  let redValue = map(xPos, 0, width, 0, 255)
+  let greenValue = map(xPos, 0, width, 0, 255)
+  fill(redValue, greenValue, 100)
+  square(xPos - radius, yPos - radius, diameter)
+  image(img, xPos - radius, yPos - radius, diameter, diameter)
+
+  xPos += xSpeed
+  yPos += ySpeed
+
+  let isPastRightSide = width < xPos + radius
+  let isPastLeftSide = 0 > xPos - radius
+  if (isPastLeftSide || isPastRightSide) {
+    xSpeed = -xSpeed;
+  }
+
+  let isBelowBottom = height < yPos + radius
+  let isAboveTop = 0 > yPos - radius
+  if (isBelowBottom || isAboveTop) {
+    ySpeed = -ySpeed
+  }
+
+}
