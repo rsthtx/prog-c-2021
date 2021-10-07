7555294de637791346ebb7ac4ff7fd747359c0b5
Rasmus Stougaard <rst@eucnordvest.dk>
2021-10-07 13:54:33 +0200

    Add logic demo

--- logic-demo/sketch.js
+++ logic-demo/sketch.js
@@ -2,13 +2,23 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
 }
 
-
 function draw() {
   background(220);
-  let x = mouseX
   let y = mouseY
+  let x = mouseX
   let diameter = 80
-  
+
+  let halfWidth = width / 2
+  let halfHeight = height / 2
+
+  let isLeft = x < halfWidth
+  let isBelow = y > halfHeight
+
+  if ((isLeft && isBelow) || ((!isLeft) && (!isBelow))) {
+    fill("red");
+  } else {
+    fill("white")
+  }
   circle(x, y, diameter)
 
 }
